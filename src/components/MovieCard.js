58524105c84f5d208397a7397f10406eb48266e7
amosmaundundu <amosmaundu5@gmail.com>
2022-09-22 11:42:55 +0300

    src

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -13,5 +13,47 @@ function MovieCard({
     </div>
   );
 }
+// // Without Destructuring
+// function MovieCard(props) {
+//   return (
+//     <div className="movie-card">
+//       <img src={props.posterSrc} alt={props.title} />
+//       <h2>{props.title}</h2>
+//       <small>{props.genres.join(", ")}</small>
+//     </div>
+//   );
+// }
+// with destructuring
+// function MovieCard ({title, posterSrc, genres}) {
+//   return (
+//    <div className="movie-card">
+//      <img src={posterSrc} alt={title}/>
+//      <h2>{title}</h2>
+//      <small> {genres.join(",")}</small>
+
+//    </div>
+//   );
+// }
+// // destructuring nested elements
+// function App() {
+//  const socialLinks = {
+//    github: "https://github.com/liza",
+//    linkedin: "https://www.linkedin.com/in/liza/",
+//  };
+
+//  return (
+//    <div>
+//      <SocialMedia links={socialLinks} />
+//    </div>
+//  );
+// }
+// function SocialMedia ({socialLinks:{ github, linkedin }}) {
+//  return (
+//    <div >
+//        <a href={github}>{github}</a>
+//      <a href={linkedin}>{linkedin}</a>
+//    </div>
+//  )
+// }
 
 export default MovieCard;
